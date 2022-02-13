8ea285454d4d9d804a3254567c289fbd75d3234b
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-02-13 18:44:18 +0000

    Add Storybook

---
+++ src/index.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from ".";
+
+export default {
+	title: "Button",
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	label: 'Button',
+	onClick: () => {},
+};
