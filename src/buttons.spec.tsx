d1ac9dcd7bfe26faa05a1605a821b7b54c8471f4
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-02-14 00:54:42 +0000

    Add secondary colourway to button

--- src/buttons.spec.tsx
+++ src/buttons.spec.tsx
@@ -16,4 +16,18 @@ describe("Button", () => {
 		userEvent.click(screen.getByRole("button"));
 		expect(onClick).toHaveBeenCalled();
 	});
+
+	it("renders a primary colourway", () => {
+		render(<Button label="" onClick={() => {}} />);
+		const primaryButton = screen.getByRole("button");
+		expect(primaryButton).toHaveStyle("background-color: #228722");
+		expect(primaryButton).toHaveStyle("color: #ffffff");
+	});
+
+	it("renders a secondary colourway", () => {
+		render(<Button label="" mode="secondary" onClick={() => {}} />);
+		const secondaryButton = screen.getByRole("button");
+		expect(secondaryButton).toHaveStyle("background-color: #ffffff");
+		expect(secondaryButton).toHaveStyle("color: #228722");
+	});
 });
--- src/buttons.tsx
+++ src/buttons.tsx
@@ -1,23 +1,39 @@
 import { FC } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export interface ButtonProps {
 	label: string;
+	mode?: "primary" | "secondary";
 	onClick: () => void;
 }
 
-const StyledButton = styled.button`
-	background-color: #228722;
+const StyledButton = styled.button<Omit<ButtonProps, "label">>`
 	border: none;
 	border-radius: 2px;
 	box-sizing: border-box;
-	color: white;
 	cursor: pointer;
 	display: inline-block;
 	font-size: 14px;
-	padding: 8px 14px;
+
+	${({ mode }) => {
+		switch (mode) {
+			case "secondary":
+				return css`
+					background-color: #ffffff;
+					border: 2px solid #228722;
+					color: #228722;
+					padding: 6px 12px;
+				`;
+			default:
+				return css`
+					background-color: #228722;
+					color: #ffffff;
+					padding: 8px 14px;
+				`;
+		}
+	}}
 `;
 
-export const Button: FC<ButtonProps> = ({ label, onClick }) => (
-	<StyledButton onClick={onClick}>{label}</StyledButton>
+export const Button: FC<ButtonProps> = ({ label, ...rest }) => (
+	<StyledButton {...rest}>{label}</StyledButton>
 );
